ba6db1117d29196cb4ade8242265d5ff647fd580
3SSI3 <3SSI3@github.com>
2023-09-21 21:49:42 +0300

    :books:

---
+++ ovidot/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Calendar from '../calendar/Calendar'
+import FAQs from '../../components/FAQs'
+import './dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <>
+    <Calendar />
+    <FAQs />
+    </>
+  )
+}
+
+export default Dashboard
---
+++ ovidot/src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+//import React from 'react'
+import MainHeader from '../../components/MainHeader'
+import './home.css'
+import Footer from '../../components/Footer'
+import FAQs from '../../components/FAQs'
+import SectionHead from '../../components/SectionHead'
+
+const Home = () => {
+  return (
+    <>
+    <MainHeader />
+    <SectionHead />
+    <FAQs />
+    <Footer />
+    </>
+  )
+}
+
+export default Home
