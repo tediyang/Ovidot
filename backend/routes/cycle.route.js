4a02b78a92e0e6a32009a5dc1b0d0caeddf5ec37
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-27 23:19:45 +0100

    changed import route

--- backend/routes/cycle.route.js
+++ backend/routes/cycle.route.js
@@ -1,26 +1,22 @@
 // cycle.route.js
-
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
 const cycleController = require('../controllers/cycle.controller');
-const verify = require('../middleWare/tokenVerification');
-
-/* protect routes */
-router.use('/:userId/cycles', verify);
+const verify = require('../middleware/tokenVerification');
 
 /**
  * create
- * gell all
+ * get all
  * get by cycleId
  * get by month
  * update
  * delete
  **/
-router.post('/:userId/cycles', cycleController.create);
-router.get('/:userId/cycles', cycleController.fetchAll);
-router.get('/:userId/cycles/:cycleId', cycleController.fetchOne);
-router.get('/:userId/cycles/:month', cycleController.fetchMonth);
-router.put('/:userId/cycles/:cycleId', cycleController.update);
-router.delete('/:userId/cycles/:cycleId', cycleController.delete);
+router.post('/:userId/cycles', verify, cycleController.create);
+router.get('/:userId/cycles', verify, cycleController.fetchAll);
+router.get('/:userId/cycles/:cycleId', verify, cycleController.fetchOne);
+router.get('/:userId/cycles/:month', verify, cycleController.fetchMonth);
+router.put('/:userId/cycles/:cycleId', verify, cycleController.update);
+router.delete('/:userId/cycles/:cycleId', verify, cycleController.delete);
 
 module.exports = router;
