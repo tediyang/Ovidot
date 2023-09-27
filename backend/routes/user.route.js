39a3a8c563f7ab4d848072efdac4e04cba1d91cb
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-27 23:20:41 +0100

    created delete route for user

---
+++ backend/routes/user.route.js
@@ -0,0 +1,17 @@
+// user.route.js
+
+const { Router } = require('express');
+const router = Router();
+const userController = require('../controllers/user.controller');
+const verify = require('../middleware/tokenVerification');
+
+/**
+ * update user data
+ * get user data
+ * delete user
+ */
+router.get('/:userId', verify, userController.fetch);
+router.put('/:userId', verify, userController.update);
+router.delete('/:userId', verify, userController.delete);
+
+module.exports = router;
