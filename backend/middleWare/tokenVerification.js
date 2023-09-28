a2eda07c26bb05b742c27b18b28f4f3ad785bf00
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-28 21:25:35 +0100

    added blacklist verification

--- backend/middleWare/tokenVerification.js
+++ backend/middleWare/tokenVerification.js
@@ -1,19 +1,27 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { handleResponse } = require('../utility/handle.response');
+const { isTokenBlacklisted } = require('./tokenBlacklist');
+
 
 const tokenVerification = (req, res, next) => {
   let token = req.header('Authorization');
 
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return handleResponse(res, 401, 'Unauthorized');
   }
 
   const secretKey = process.env.SECRETKEY;
   token = token.substring(7); // remove Bearer
 
+// Check if the token is blacklisted
+  if (isTokenBlacklisted(token)) {
+    return handleResponse(res, 401, 'Invalid token');
+  }
+
   jwt.verify(token, secretKey, (err, user) => {
     if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return handleResponse(res, 401, 'Invalid token');
     }
 
     req.user = user;
