4b435e02b0ea44e758236a6bb04837f1813911f2
mdekabs <1mawoda@gmail.com>
2023-09-29 22:11:25 +0100

    admin

---
+++ backend/Admin/route/admin.route.js
@@ -0,0 +1,21 @@
+/* route */
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controller/admin.controller');
+const verify = require('../../middleWare/tokenVerification');
+/**
+ * create new admin
+ * delete admin
+ * view all cycle
+ * view by Id
+ * edit cycle
+ * delete cycle
+ **/
+router.post('/create', adminController.createAdmin);
+router.delete('/delete/:adminId', verify, adminController.deleteAdmin);
+router.get('/view-all-cycle-data', verify, adminController.viewAllCycleData);
+router.get('/view-specific-cycle-data/:cycleId', verify, adminController.viewSpecificCycleData);
+router.put('/edit-cycle-data/:cycleId', verify, adminController.editCycleData);
+router.delete('/delete-cycle-data/:cycleId', verify, adminController.deleteCycleData);
+
+module.exports = router;
