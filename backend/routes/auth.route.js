4bfcdc70b9ebe90ad4761c4f0d1b983623859751
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-29 18:34:45 +0100

    added validation for signup and created change password for logged in user

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 const authController = require('../controllers/auth.controller.js');
 const { body } = require('express-validator');
-
 const router = Router();
+const verify = require('../middleware/tokenVerification');
 
 // Signup
 router.post('/signup', [
     body("email").isString().notEmpty(),
-    body("password").isString().notEmpty()
+    body("password").isString().notEmpty(),
+    body("username").isString().notEmpty(),
+    body("age").isNumeric().notEmpty()
     ],
     authController.signup
 );
@@ -20,6 +22,14 @@ router.post('/login', [
     authController.login
 );
 
-router.get('/logout', authController.logout);
+// Logout
+router.get('/logout', verify, authController.logout);
+
+// Change Password
+router.post('/change-password', [
+    body("currentPassword").isString().notEmpty(),
+    body("newPassword").isString().notEmpty(),
+    ],
+    verify, authController.changePass);
 
 module.exports = router;
