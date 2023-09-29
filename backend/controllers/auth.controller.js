6dcbcb7038775f748db6b9dcdd000f6ed41ebd9a
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-29 18:31:38 +0100

    added change password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,26 +9,39 @@ const { updateBlacklist } = require('../middleware/tokenBlacklist');
 // Secret key for jwt signing and verification
 const secretKey = process.env.SECRETKEY;
 
-// Generate a jwt token for a user when successfullly logged in
+/**
+ * create a token for a user that expires 1hr
+ * @param {User} user - user object 
+ * @returns - token
+ */
 function createToken(user) {
   return jwt.sign({ id: user._id, email: user.email }, secretKey, { expiresIn: '1h' });
 }
 
-// Create User account when for sign up
+/**
+ * Create an account for a user
+ * @param {Object} req 
+ * @param {Object} res 
+ * @returns - status 200 and payload
+ */
 exports.signup = async (req, res) => {
   // Validate the data
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return handleResponse(res, 400, "Fill required properties");
   }
 
-  // console.log('yes');
   const { email, password, username, age } = req.body;
 
   return await userController.create({ email, password, username, age }, res);
 }
 
-// Login route to generate a JWT token
+/**
+ * Login a user and generate a token
+ * @param {Object} req 
+ * @param {Object} res 
+ * @returns - status 200 and payload
+ */
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email: email });
@@ -54,14 +67,61 @@ exports.login = async (req, res) => {
   }
 };
 
+/**
+ * Logout a user and expire the token
+ * @param {Object} req 
+ * @param {Object} res 
+ * @returns - status 200
+ */
 exports.logout = async (req, res) => {
-  let token = req.header('Authorization');
+  try {
+    let token = req.header('Authorization');
 
-  // if token is present then update to the blacklist
-  if (token) {
-    token = token.substring(7); // remove Bearer
-    updateBlacklist(token);
+    // if token is present then update to the blacklist
+    if (token) {
+      token = token.substring(7); // remove Bearer
+      updateBlacklist(token);
+    }
+
+    return handleResponse(res, 200, 'logged out successfully');
+  } catch (error) {
+    return handleResponse(res, 500, 'Internal Server Error');
   }
+}
 
-  return handleResponse(res, 200, 'logged out successfully');
+/**
+ * Change logged-in user password
+ * @param {Object} req 
+ * @param {Object} res 
+ * @returns - Success
+ */
+exports.changePass = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
+
+    if (!isPasswordValid) {
+      return handleResponse(res, 400, 'Current password is incorrect');
+    }
+
+    const saltRounds = 12;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+
+    user.password = hashedNewPassword;
+    await user.save();
+
+    res.status(200).json({ message: 'Password changed successfully' });
+  } catch (error) {
+    console.log(error);
+    return handleResponse(res, 500, 'Internal Server Error');
+  }
 }
