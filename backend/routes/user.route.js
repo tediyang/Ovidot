d36a6d32aad5d85874d2d6b1e9c4ffe3a54190d6
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-09-30 23:53:57 +0100

    validate user put route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,14 +4,25 @@ const { Router } = require('express');
 const router = Router();
 const userController = require('../controllers/user.controller');
 const verify = require('../middleware/tokenVerification');
+const { body } = require('express-validator');
 
 /**
  * update user data
  * get user data
  * delete user
  */
+
+// Get user data
 router.get('/:userId', verify, userController.fetch);
-router.put('/:userId', verify, userController.update);
+
+// Update user data
+router.put('/:userId', [
+    body('username').isString(),
+    body('age').isNumeric()
+    ],
+    verify, userController.update);
+
+// Delete user
 router.delete('/:userId', verify, userController.delete);
 
 module.exports = router;
