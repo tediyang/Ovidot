d7f201b7e4a6151c5c3702a83ab79a24e3425b30
mdekabs <1mawoda@gmail.com>
2023-10-28 17:34:23 +0100

    db

--- backend_v2/utils/db.js
+++ backend_v2/utils/db.js
@@ -3,14 +3,8 @@ const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 27017;
 const DB = process.env.DB || "test";
 const uri = `mongodb://${HOST}:${PORT}/${DB}`;
-const User = require('../models/user.model');
-const Cycle = require('../models/cycle.model');
-
-const client = new mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
+const User = require('../models/user.model'); // Assuming these are Mongoose models
+const Cycle = require('../models/cycle.model'); // Assuming these are Mongoose models
 
 class DBClient {
   constructor() {
@@ -18,27 +12,24 @@ class DBClient {
   }
 
   async initialize() {
-
-    try { 
-      await client;
-    }
+    try {
+      await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
 
       this.db = mongoose.connection;
       this.userCollection = User;
       this.cycleCollection = Cycle;
-      }
-      catch(error) {
-
-        console.log("Error connecting to MongoDB:", error);
-
-        this.db = null;
-      }
+    } catch (error) {
+      console.log("Error connecting to MongoDB:", error);
+      this.db = null;
+    }
   }
 
   isAlive() {
     return Boolean(this.db);
   }
-  
 
   /* return the number of docs in users collection */
   async nbUsers() {
@@ -49,24 +40,20 @@ class DBClient {
     try {
       const numberOfUsers = await this.userCollection.countDocuments();
       return numberOfUsers;
-    }
-    catch(error) {
+    } catch (error) {
       console.error("Error counting users:", error);
       throw error;
     }
   }
-
 
   async nbCycle() {
     if (!this.db) {
       throw new Error("DB connection not established.");
     }
     try {
-
       const numberOfCycles = await this.cycleCollection.countDocuments();
       return numberOfCycles;
-    }
-    catch(error) {
+    } catch (error) {
       console.error("Error counting cycles:", error);
       throw error;
     }
