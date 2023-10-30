13d696ef5ac4f27b23b2091c096b3efbd9ea9436
mdekabs <1mawoda@gmail.com>
2023-10-30 14:36:04 +0100

    fixed

--- backend_v2/utils/user.js
+++ backend_v2/utils/user.js
@@ -4,17 +4,16 @@ const dbClient = require("./db");
 /* user utilities */
 const userUtils = {
   /**
-   * get a user id and key from redis req
+   * get a user id and key from req
    * @req {req_obj}
-   * @return{oject= userId:key}
+   * @return {object} userId and key
    */
-
   async getUserIdAndKey(req) {
-    const obj = {userId: null, key: null};
-    const xToken = request.header("x-Token");
-    if (!xToken)
+    const obj = { userId: null, key: null };
+    const xToken = req.header("x-Token");
+    if (!xToken) {
       return obj;
-
+    }
 
     obj.key = `auth_${xToken}`;
     obj.userId = await redisClient.get(obj.key);
@@ -22,19 +21,19 @@ const userUtils = {
   },
 
   /**
-   * get user from db
-   * @query {obj} expression to finding user
-   * @return {obj} user doc
+   * get user from the database
+   * @param {object} query - expression to find a user
+   * @return {object} - user document
    */
   async getUser(query) {
-    const user = await dbClient.userCollection.fineOne(query).exec();
-    return user;
-  }
-  catch (error) {
-    console.error("error fetching user", error);
-    throw error;
-  }
-},
+    try {
+      const user = await dbClient.userCollection.findOne(query).exec();
+      return user;
+    } catch (error) {
+      console.error("Error fetching user", error);
+      throw error;
+    }
+  },
 };
 
 module.exports = userUtils;
