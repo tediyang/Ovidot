bafc7ca620cb007d301014852a720bbc9e65a530
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-11-28 13:57:34 +0100

    setup logging

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,13 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import verify from './v1/middleware/tokenVerification.js';
+import { logger, appLogger} from './v1/middleware/logger.js';
 dotenv.config();
 
 // Import routes
 import generalRoutes from './v1/routes/general.routes.js';
 import authRoutes from './v1/routes/auth.routes.js';
 import adminRoutes from './v1/admin/route/admin.routes.js';
-
-// Import middlewares
-// import loggerMiddleware from './v1/middleware/logger.middleware.js';
-// import errorHandle from './v1/middleware/error.middleware.js';
 
 const { connect, connection } = mongoose;
 const { urlencoded } = bodyParser;
@@ -36,25 +33,28 @@ connect(`mongodb://${HOST}/${DB}`, {
 
 const db = connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', error => {
+  logger.error('MongoDB connection error:', error);
+});
 db.once('open', () => {
-  console.log('MongoDB connected!');
+  logger.info('MongoDB connected!');
 });
 
 app.use(cors());
 app.use(urlencoded({ extended: false }));
 app.use(json());
 
+// use loggers
+app.use(appLogger);
+
 // use routes
 app.use(APP_PATH+'/auth', verify, authRoutes);
 app.use(APP_PATH+'/admin', adminRoutes);
 app.use(APP_PATH, generalRoutes);
 
-// app.use(loggerMiddleware);
-// app.use(errorHandle);
 
 app.listen(PORT, () => {
-  console.log(`Server is now running on port ${PORT}`);
+  logger.info(`Server is now running on port ${PORT}`);
 });
 
 export default app;
