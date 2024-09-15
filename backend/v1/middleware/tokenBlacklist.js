d13b952f6d89b4b7e0f8a207f2f9087581eee226
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2024-09-15 17:07:54 +0100

    setup Blacklist class

--- backend/v1/middleware/tokenBlacklist.js
+++ backend/v1/middleware/tokenBlacklist.js
@@ -1,41 +1,54 @@
-import { readFileSync, writeFileSync } from 'fs';
-import dotenv from 'dotenv';
-dotenv.config();
+const { readFileSync, writeFileSync } = require('fs');
+require('dotenv').config();
 
-const blacklist = process.env.BLACKLIST;
 
-/**
- * Fuction that reads the blacklist json file
- * @returns - An array of tokens or an empty list.
- * 
- */
-function readBlacklist() {
-  try {
-    const data = readFileSync(blacklist, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    return [];
+class BlackList {
+  /**
+   * Initializes the BlackList instance.
+   *
+   * @constructor
+   */
+  constructor() {
+    this._blacklist = process.env.BLACKLIST;
+  };
+
+  /**
+   * Fuction that reads the blacklist json file
+   * @returns - An array of tokens or an empty list.
+   * 
+   */
+  readBlacklist() {
+    try {
+      const data = readFileSync(this._blacklist, 'utf-8');
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
   }
-}
+  
+  /**
+   * blacklist a token
+   * @param {String} token - User token
+   */
+  updateBlacklist(token) {
+    const blacklistData = this.readBlacklist();
+    blacklistData.push(token);
+  
+    // Write the updated blacklist array back to the file
+    writeFileSync(this._blacklist, JSON.stringify(blacklistData));
+  }
+  
+  /**
+   * Checks if a token is blacklisted
+   * @param {String} token - the user token for authorization 
+   * @returns - True - if the token is blacklisted and False - if it isn't.
+   */
+  isTokenBlacklisted(token) {
+    const blacklistData = this.readBlacklist();
+    return blacklistData.includes(token);
+  }
+};
 
-/**
- * Updated a token to the blacklist
- * @param {String} token - User token
- */
-export function updateBlacklist(token) {
-  const blacklistData = readBlacklist();
-  blacklistData.push(token);
 
-  // Write the updated blacklist array back to the file
-  writeFileSync(blacklist, JSON.stringify(blacklistData));
-}
-
-/**
- * Checks if a token is blacklisted
- * @param {String} token - the user token for authorization 
- * @returns - True - if the token is blacklisted and False - if it isn't.
- */
-export function isTokenBlacklisted(token) {
-  const blacklistData = readBlacklist();
-  return blacklistData.includes(token);
-}
+const blacklist = new BlackList()
+module.exports = blacklist;
