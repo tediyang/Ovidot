29a9a3efe19c6cd3a9a7e3be5705a4286c9dbef9
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2024-09-15 16:26:16 +0100

    restructure routes, changed get users param

--- backend/v1/controllers/user.controller.js
+++ backend/v1/controllers/user.controller.js
@@ -1,163 +1,418 @@
 // USER CONTROLLER (CRUD)
-import bcrypt from 'bcryptjs';
-import User from '../models/user.model.js';
-import { handleResponse } from '../utility/handle.response.js';
-import notifications from '../services/notifications.js';
-import { validationResult } from 'express-validator';
+const handleResponse = require('../utility/helpers/handle.response.js');
+const notifications = require('../services/notifications.js');
+const requestValidator = require('../utility/validators/requests.validator.js')
+const { emailType } = require('../enums.js');
+const util = require('../utility/encryption/cryptography.js')
+const { Connection, Email, User, MongooseError } = require('../models/engine/database.js');
+const { userAction, notificationStatus, userStatus } = require('../enums.js');
+const { Types } = require('mongoose');
+const Joi = require('joi');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const blacklist = require('../middleware/tokenBlacklist.js');
+require('dotenv').config();
 
-const { genSalt, hash } = bcrypt;
 
-/**
- * Create the user object for the new user if it doesn't exist
- * @data - user data passed, used for creating a user.
- * @res - Express Response
- * @returns Payload on success
- */
-export async function createUser(data, res) {
-	// create a user
-	const email = data.email
-	const existUser = await User.findOne({ email });
-	if (existUser) {
-		return handleResponse(res, 404, `${email} already exist.`);
+class UserController {
+
+	_EXCLUDE = process.env.EXCLUDE;
+	/**
+	 * Create the user object for the new user if it doesn't exist
+	 * @res - Express Response
+	 * @data - user data passed, used for creating a user.
+	 * @returns Payload on success
+	 */
+	async createUser(res, data) {
+		try {
+			// check for existing user
+			const existing_data = Promise.all([
+				User.findOne({ email: data.email }),
+				User.findOne({ phone: data.phone })
+			])
+	
+			const [email, phone] = await existing_data
+	
+			if (email || phone) {
+				if (email) {
+					return handleResponse(res, 400, 'Email already exists');
+				}	else {
+					return handleResponse(res, 400, 'Phone already exists');
+				}
+			}
+	
+			// check for min age (8 years)
+			const minDOB = new Date(); // minimum age
+			const maxDOB = new Date(); // maximum age
+			minDOB.setFullYear(minDOB.getFullYear() - 8);
+			maxDOB.setFullYear(maxDOB.getFullYear() - 58);
+			if (data.dob > minDOB) {
+				return handleResponse(res, 400, 'You are too young to menstrate');
+			}
+			else if (data.dob < maxDOB) {
+				return handleResponse(res, 400, 'You are above the menstrual age');
+			}
+	
+			// create user
+			const user = {
+				name: {
+					fname: data.fname,
+					lname: data.lname
+				},
+				email: data.email,
+				phone: data.phone,
+				username: data.username,
+				dob: data.dob,
+				period: data.period
+			};
+	
+			// encrypt user password
+			const pwd = await util.encrypt(data.password);
+			user.password = pwd;
+	
+			await Connection.transaction(async () => {
+				const resoled_u = await User.create(user);
+	
+				// Send email
+				await Email.create({
+					email: user.email,
+					username: user.username,
+					email_type: emailType.welcome
+				});
+	
+				// Generate notification
+				const message = `${user.username}, your account has been created`;
+				const notify = await notifications.generateNotification(userAction.createdUser, message);
+	
+				// Add the notification
+				resoled_u.notificationsList.push(notify);
+				await resoled_u.save();
+			});
+	
+			return handleResponse(res, 201, "Registration Successful");
+	
+		} catch (error) {
+			throw error;
+		}
+	};
+
+	/**
+	 * Find the user and update the data passed.
+	 * @param {Object} req - Express Request
+	 * @param {Object} res - Express Response
+	 * @returns - updated user object
+	 */
+	async updateUser(req, res) {
+		try {
+			// validate user input
+			const { value, error } = requestValidator.UpdateUser.validate(req.body);
+	
+			if (error) {
+				throw error;
+			};
+	
+			const minDOB = new Date();
+			const maxDOB = new Date();
+			minDOB.setFullYear(minDOB.getFullYear() - 8);
+			maxDOB.setFullYear(maxDOB.getFullYear() - 58);
+			if (value.dob > minDOB || value.dob < maxDOB) {
+				return handleResponse(res, 400, 'You are too young or above the age to menstrate (min: 8 years, max: 58 years)');
+			};
+
+	
+			const { fname, lname, dob, period, username, sensitive, password } = value;
+	
+			const user = await User.findById(req.user.id, this._EXCLUDE);
+			if(!user) {
+				return handleResponse(res, 404, "User not found");
+			}
+	
+			// validates password for updating sensitive data
+			if (sensitive) {
+				const { phone, new_password } = sensitive;
+				const is_pwd = await util.validate_encryption(password, user.password);
+				// validate password
+				if(!is_pwd) {
+					return handleResponse(res, 400, "Invalid password");
+				}
+
+				if (phone) {
+					user.phone = phone;
+				}
+				if (new_password) {
+					user.password = await util.encrypt(new_password);
+				}
+			}
+	
+			if(fname) { user.name.fname = fname; }
+			if(lname) { user.name.lname = lname; }
+			if(dob) { user.dob = dob; }
+			if(period) { user.period = period; }
+			if(username) { user.username = username; }
+	
+			// Generate notification
+			const message = 'Your profile has been updated';
+			const notify = await notifications.generateNotification(userAction.updatedUser, message);
+	
+			// Add new notification
+			user.notificationsList.push(notify);
+	
+			// manage noifications
+			await notifications.manageNotification(user.notificationsList);
+			await user.save();
+	
+			return res
+				.status(200)
+				.json({
+					message: 'User succesfully updated',
+					user: user,
+				});
+	
+		} catch(error) {
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof Joi.ValidationError) {
+				return handleResponse(res, 400, error.details[0].message);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error)
+		}
 	}
 
-	try {
-		const saltRounds = 12;
-		const salt = await genSalt(saltRounds);
-		// Hash the password
-		const hashedPassword = await hash(data.password, salt);
-				data.password = hashedPassword;
-		// Register the new user data. The create method prevents sql injection
-		const newUser = await User.create(data);
-
-		// create notification
-		const message = `${newUser.username}, your account has been created`;
-		const notify = notifications.generateNotification(newUser, 'createdUser', message);
-
-		// Add new notification
-		newUser.notificationsList.push(notify);
-
-		await newUser.save();
-
-		// send email notification
-		await notifications.sendUserCreationNotification(newUser);
-		return res.status(201).send();
-
-	} catch (error) {
-		return handleResponse(res, 500, 'Internal Server Error', error);
+	/**
+	 * Fetch and return the user data from the database.
+	 * @param {Object} req - Express Request
+	 * @param {Object} res - Express Response
+	 * @returns -  the user data.
+	 */
+	async fetchUser(req, res) {
+		try {
+			const user = await User.findById(req.user.id, this._EXCLUDE);
+	
+			if(!user) {
+				return handleResponse(res, 404, "User not found");
+			}
+	
+			return res.status(200).json({ user });
+	
+		} catch(error) {
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error)
+		}
 	}
-};
 
-/**
- * Find the user and update the data passed.
- * @param {Object} req - Express Request
- * @param {Object} res - Express Response
- * @returns - updated user object
- */
-export async function updateUser(req, res) {
-	// get userId from params
-	try {
-		// validate the params
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return handleResponse(res, 400, errors.array()[0].msg);
+
+	/**
+	 * Deactivate a user account.
+	 * @param {Object} req - Express Request
+	 * @param {Object} res - Express Response
+	 * @returns {void}
+	 * @throws {Object} - Error response object.
+	 */
+	async deactivateUser(req, res) {
+		try {
+			const user = await User.findById(req.user.id);
+			if (!user) {
+				return handleResponse(res, 404, "User not found");
+			}
+			
+			user.status = userStatus.deactivated;
+
+			await Connection.transaction(async () => {
+				// Send termination email
+				await Email.create({
+					email: user.email,
+					username: user.username,
+					email_type: emailType.deactivate
+				});
+
+				// Generate notification
+				const message = 'Your profile has been deactivated';
+				const notify = await notifications.generateNotification(userAction.deactivatedUser, message);
+		
+				// Add new notification
+				user.notificationsList.push(notify);
+
+				// blacklist token
+				blacklist.updateBlacklist(req.token);
+		
+				// manage noifications
+				await notifications.manageNotification(user.notificationsList);
+				await user.save();
+
+			});
+			return handleResponse(res, 204);
+	
+		} catch (error) {
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error);
 		}
+	};
 
-		const userId = req.user.id;
-		const { username, age, period } = req.body;
+	/**
+	 * Delete the user data from the database.
+	 * @param {Object} res - response sent to user
+	 * @param {Object} req - request from user
+	 * @returns - Payload on Success
+	 */
+	async deleteUser(req, res) {
+		try {
+			await Connection.transaction(async () => {
+				const user = await User.findByIdAndDelete(req.user.id);
+				if (!user) {
+					throw new Error("User not found");
+				}
 
-		if (age <= 0 || period <= 0) {
-			return handleResponse(res, 400, "Invalid value");
+				// blacklist token
+				blacklist.updateBlacklist(req.token);
+
+				// Send termination email
+				await Email.create({
+					email: user.email,
+					username: user.username,
+					email_type: emailType.delete
+				});
+			});
+	
+			return handleResponse(res, 204);
+	
+		} catch (error) {
+			if (error.message === "User not found") {
+				return handleResponse(res, 404, error.message);
+			}
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error);
 		}
+	};
 
-		if (!username && !age && !period) {
-			return handleResponse(res, 400, "Provide atleast a param to update: username, period or age");
+	/**
+	 * Get a notification and mark it as read.
+	 * @param {Object} req - Express request object.
+	 * @param {Object} res - Express response object.
+	 * @returns {Promise<Object>} A Promise that resolves to the updated notification.
+	 * @throws {Object} If the user is not found, the notification is not found, or there is a Mongoose or JSON Web Token error.
+	 */
+  async getNotification(req, res) {
+    try {
+      if (!req.params.id) {
+				return handleResponse(res, 400, 'Invalid request, id is required');
+      }
+
+      const user = await User.findById(req.user.id);
+      if (!user) {
+        return handleResponse(res, 404, 'User not found');
+      };
+
+      const note = user.notificationsList.id(new Types.ObjectId(req.params.id));
+      if (!note) {
+				return handleResponse(res, 404, 'Notification not found');
+      }
+
+			note.status = notificationStatus.read;
+
+      await user.save();
+
+      return res
+        .status(200)
+        .json({ note });
+
+    } catch (error) {
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error)
 		};
+  }
+	
+	/**
+	 * Get all the notifications of the user
+	 * @param {Object} req - Express Request
+	 * @param {Object} res - Express Response
+	 * @return {Promise<void>}
+	 */
+	async getNotifications(req, res) {
+		try {
+			const user = await User.findById(req.user.id);
+			if(!user) {
+				return handleResponse(res, 404, "User not found")
+			};
 
-		const updatedAt = new Date();
-		const user = await User.findByIdAndUpdate(userId,
-      {username: username, age: age, period: period, updated_at: updatedAt},
-			{new: true});
+			return res
+			.status(200)
+			.json({
+					notifications: user.notificationsList
+				});
+		} catch (error) {
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error)
+		}
+	}
 
-		/* check for conditons */
-		if(!user) {
-			return handleResponse(res, 404, "User not found");
+	/**
+	 * Delete a notification of the user
+	 * @param {Object} req - Express Request
+	 * @param {Object} res - Express Response
+	 * @return {Promise<void>}
+	 */
+	async deleteNotification(req, res) {
+		try {
+			if (!req.params.id) {
+				return handleResponse(res, 400, 'Invalid request, id is required');
+			}
+
+			const user = await User.findById(req.user.id);
+			if (!user) {
+				return handleResponse(res, 404, 'User not found');
+			}
+
+			const note = user.notificationsList.id(new Types.ObjectId(req.params.id));
+			if (!note) {
+				return handleResponse(res, 404, 'Notification not found');
+			}
+
+			user.notificationsList = user.notificationsList.filter(data => (data._id !== note._id));
+
+			await user.save();
+
+			return handleResponse(res, 204);
+
+		} catch (error) {
+			if (error instanceof MongooseError) {
+				return handleResponse(res, 500, "We have a mongoose problem", error);
+			}
+			if (error instanceof JsonWebTokenError) {
+				return handleResponse(res, 500, error.message, error);
+			}
+			return handleResponse(res, 500, error.message, error)
 		};
-
-		// create notification
-		const message = 'Your profile has been updated'
-
-		const notify = notifications.generateNotification(user, 'updatedUser', message);
-
-		// Add new notification
-		user.notificationsList.push(notify);
-
-		// manage noifications
-		notifications.manageNotification(user.notificationsList);
-
-		await user.save();
-
-		// else return OK response
-		res.status(200).json({
-			userId: user._id,
-			email: user.email,
-			username: user.username,
-			age: user.age,
-			period: user.period
-		});
-	} catch(error) {
-		return handleResponse(res, 500, "Internal server error", error);
 	}
 }
 
-/**
- * Fetch and return the user data from the database.
- * @param {Object} req - Express Request
- * @param {Object} res - Express Response
- * @returns -  the user data.
- */
-export async function fetchUser(req, res) {
-	try {
-		const userId = req.user.id;
-		const user = await User.findById(userId);
 
-		/* check for conditons */
-		if(!user) {
-			return handleResponse(res, 404, "User not found");
-		}
-
-		return res.status(200).json({
-			userId: user._id,
-			email: user.email,
-			username: user.username,
-			age: user.age,
-			period: user.period
-		});
-
-	} catch(error) {
-		return handleResponse(res, 500, "Internal server error", error);
-	}
-}
-
-/**
- * Delete the user data from the database.
- * @param {Object} res - response sent to user
- * @param {Object} req - request from user
- * @returns - Payload on Success
- */
-export async function deleteUser(req, res) {
-	try {
-		const userId = req.user.id;
-		const user = await User.findByIdAndDelete(userId);
-		if (!user) {
-			return handleResponse(res, 404, "User not found");
-		}
-
-		// send email notification
-		await notifications.sendUserTerminationNotification(user);
-
-		return res.status(204).send();
-	} catch (error) {
-		handleResponse(res, 500, "Internal server error", error);
-	}
-};
+const userController = new UserController();
+module.exports = userController;
