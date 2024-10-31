e27ea826946bb0693f92d875ed9b1f4ea76e80be
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2024-10-31 20:05:39 +0100

    setup refresh token method

--- backend/v1/controllers/register.controller.js
+++ backend/v1/controllers/register.controller.js
@@ -1,6 +1,6 @@
 // REGISTER CONTROLLER
 const { User, MongooseError } = require('../models/engine/database.js');
-const { sign, JsonWebTokenError } = require('jsonwebtoken');
+const { sign, JsonWebTokenError, verify } = require('jsonwebtoken');
 const { compare } = require('bcrypt');
 const { PATH_PREFIX } = require('../swagger-docs')
 const userController = require('./user.controller.js');
@@ -16,13 +16,26 @@ const secretKey = process.env.SECRETKEY;
 
 
 class AppController {
-  /**
-   * Generate token for user. Expiration 5hrs
-   * @param {User} user - User object to generate token for
+    /**
+   * Generates a token based on the provided payload.
+   *
+   * @param {Object} payload - The payload used to generate the token.
+   * @param {boolean} just_access=false - Determines if only the access token should be generated.
+   * @return {string|Object} - The generated token(s).
    */
-  createToken(user) {
-    return sign({ id: user._id, email: user.email, status: user.status }, secretKey, { expiresIn: '5h' });
-  };
+    async createToken(payload, just_access=false) {
+      try {
+        if(just_access) {
+          const accessToken = sign({ id: payload._id, email: payload.email, status: payload.status }, secretKey, { expiresIn: '5h' });
+          return accessToken;
+        }
+        const accessToken = sign({ id: payload._id, email: payload.email, status: payload.status }, secretKey, { expiresIn: '5h' });
+        const refreshToken = sign({id: payload._id}, secretKey, { expiresIn: '7d' });
+        return { accessToken, refreshToken };
+      } catch (error) {
+        throw error;
+      }
+    }
 
   /**
    * Register user
@@ -106,17 +119,18 @@ class AppController {
         })
       }
 
-      const token = this.createToken(user);
+      const tokens = await this.createToken(user);
 
       // reset login attempts
       user.loginAttempts = 0;
+      user.jwtRefreshToken = tokens.refreshToken;
       await user.save();
 
       return res
         .status(200)
         .json({
           message: 'Authentication successful',
-          token
+          tokens
         });
   
     } catch (error) {
@@ -139,14 +153,19 @@ class AppController {
    * @param {Object} res - Express Response
    * @return Payload on Success
    */
-  logout(req, res) {
+  async logout(req, res) {
     try {
       let token = req.header('Authorization');
-  
+      const user = await User.findById(req.user.id);
+
       // if token is present then update to the blacklist
       if (token) {
         token = token.substring(7); // remove Bearer
         blacklist.updateBlacklist(token);
+
+        // reset refresh token
+        user.jwtRefreshToken = '';
+        await user.save();
       }
   
       return handleResponse(res, 200, "Logout Successful");
@@ -154,6 +173,81 @@ class AppController {
       return handleResponse(res, 500, error.message, error);
     }
   };
+
+  /**
+ * Refreshes the access token for a user.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Object} The response containing the new access token and user information.
+ */
+  async refreshToken(req, res) {
+    try {
+      // validate body
+      const { token } = req.params;
+      let userPayload;
+  
+      if (!token) return handleResponse(res, 401, 'Requires a token');
+      // validate refresh_token
+      try {
+        userPayload = await new Promise((resolve, reject) => {
+          verify(token, process.env.SECRETKEY, (err, user) => {
+            if (err) return reject(err);
+            resolve(user);
+          });
+        });
+      } catch (error) {
+        return res.status(401).json({
+          message: 'Invalid Credential, Refresh token invalid',
+          second_chance: false,
+        });
+      }
+
+      const user = await User.findById(userPayload.id, 'jwtRefreshToken email _id status').exec();
+			if(!user) {
+				return handleResponse(res, 404, "User not found");
+			}
+
+      // validate refresh user's refresh_token
+      if(user.jwtRefreshToken !== token) {
+        return handleResponse(res, 400, "Invalid Credential, Refresh token invalid");
+      }
+
+      if (user.status !== userStatus.active) {
+        const resolve = `${PATH_PREFIX}/general/forget-password/`;
+        return res.status(401).json({
+          message: "Account Deactivated",
+          resolve
+        });
+      };
+
+      // refresh access token
+      const newAccessToken = await this.createToken({
+        id: user._id,
+        email: user.email,
+        status: user.status
+      }, true);
+
+      return res
+        .status(200)
+        .json({
+          message: 'Token refresh successful',
+          token: newAccessToken,
+        });
+
+      } catch (error) {
+        if (error instanceof MongooseError) {
+          return handleResponse(res, 500, "We have a mongoose problem", error);
+        }
+        if (error instanceof Joi.ValidationError) {
+          return handleResponse(res, 400, error.details[0].message);
+        }
+        if (error instanceof JsonWebTokenError) {
+          return handleResponse(res, 500, error.message, error);
+        }
+        return handleResponse(res, 500, error.message, error)
+      }
+  }
 }
 
 const appController = new AppController()
