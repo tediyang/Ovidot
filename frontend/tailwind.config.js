aa53fead7c0bfd6df64b4aa644adc4fd1493a401
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2024-11-20 14:14:56 +0100

    Tediyang frontend (#25)
    * header component
    
    * reduced the logo to 22% width and sign up button min-h
    
    * created custom boxshadow - evenly

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'evenly': '0 0 5px 2px rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
 }
