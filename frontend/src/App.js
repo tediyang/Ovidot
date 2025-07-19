7afdcbcdcb5e5ec44955d3f825dabb5aa58fb14f
Eyand Daniel Eyoh <daniel.eyang.ed@gmail.com>
2025-07-19 11:41:49 +0100

    UPDATES: added about route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Help from "./pages/Help/Help";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<About />} />
         <Route path="/help" element={<Help />} />
       </Routes>
     </Router>
