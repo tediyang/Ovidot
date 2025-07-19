5c3b6bc735ced0542f92347449aaf8cc015de780
Eyand Daniel Eyoh <daniel.eyang.ed@gmail.com>
2025-07-19 11:36:30 +0100

    REFACTOR: refactor to support dynamic positioning

--- frontend/src/components/BlurEllipse.jsx
+++ frontend/src/components/BlurEllipse.jsx
@@ -1,7 +1,15 @@
-const BlurEllipse = () => {
+import { PropTypes } from "prop-types";
+
+const BlurEllipse = (props) => {
+  const { position } = props;
+
   return (
-    <div className="hidden md:block absolute -left-32 -top-6 h-[200px] w-[200px] blur-ellipse"></div>
+    <div className={`hidden sm:block absolute ${position} h-[200px] w-[200px] blur-ellipse`}></div>
   )
+}
+
+BlurEllipse.propTypes = {
+  position: PropTypes.string.isRequired
 }
 
 export default BlurEllipse;
