0e87086bf14fcbc89b63bbda3b1ec25cd2a0b02a
Eyand Daniel Eyoh <daniel.eyang.ed@gmail.com>
2025-12-01 20:06:47 +0100

    UPDATES: added settings route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ForgetPassword from "./pages/ForgetPassword/ForgetPasswordPage";
 import ResetPassword from "./pages/ForgetPassword/ResetPasswordPage";
 import Dashboard from "./pages/Dashboard/DashboardPage";
 import Profile from "./pages/Profile/ProfilePage";
+import Settings from "./pages/Settings/SettingsPage";
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
