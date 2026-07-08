4d3153d3d308b81b50426be771133c197174f884
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2026-07-08 12:38:33 +0100

    UPDATE: refactor and move cronjob to external service to call

--- backend/v1/libs/boot.js
+++ backend/v1/libs/boot.js
@@ -1,61 +1,57 @@
-const Express = require('express');
-const { logger } = require('../middleware/logger.js');
-const { schedule } = require('node-cron');
-const email_service = require('../services/emailService.js');
-const { createClient } = require('redis');
-require('dotenv').config();
+const Express = require("express");
+const { logger } = require("../middleware/logger.js");
+const { createClient } = require("redis");
+require("dotenv").config();
 
 // Get environment variables
-const {
-  REDIS_URL,
-  ENVIR,
-  PORT
-} = process.env;
-
+const { HOST, REDIS_URL, ENVIR, PORT } = process.env;
 
 const redisSetup = async () => {
   let client;
 
   try {
-    if (ENVIR === 'test' || ENVIR === 'dev') {
+    if (ENVIR === "test" || ENVIR === "dev") {
       // Local or test environment
       client = createClient({
         socket: {
-          reconnectStrategy: retries => {
+          reconnectStrategy: (retries) => {
             if (retries > 3) {
-              logger.error('Max reconnection attempts exceeded');
-              return new Error('Max reconnection attempts exceeded');
+              logger.error("Max reconnection attempts exceeded");
+              return new Error("Max reconnection attempts exceeded");
             }
             return Math.min(retries * 50, 2000);
           },
         },
       });
 
-      client.on('connect', () => logger.info('Redis Client connected on PORT 6379'));
+      client.on("connect", () =>
+        logger.info("Redis Client connected on PORT 6379"),
+      );
     } else {
       // Production environment using Render's internal Redis
       client = createClient({
         url: REDIS_URL,
         socket: {
-          reconnectStrategy: retries => {
+          reconnectStrategy: (retries) => {
             if (retries > 3) {
-              logger.error('Max reconnection attempts exceeded');
-              return new Error('Max reconnection attempts exceeded');
+              logger.error("Max reconnection attempts exceeded");
+              return new Error("Max reconnection attempts exceeded");
             }
             return Math.min(retries * 50, 2000);
           },
         },
       });
 
-      client.on('connect', () => logger.info(`Redis Client connected to ${REDIS_URL}`));
+      client.on("connect", () =>
+        logger.info(`Redis Client connected to ${REDIS_URL}`),
+      );
     }
-    client.on('error', err => logger.error('Redis Client Error', err));
+    client.on("error", (err) => logger.error("Redis Client Error", err));
 
     await client.connect(); // Connect the client
     return client;
-
   } catch (error) {
-    logger.error('Redis setup failed', error);
+    logger.error("Redis setup failed", error);
   }
 };
 
@@ -66,28 +62,13 @@ const redisSetup = async () => {
 const startServer = async (app) => {
   const port = PORT || 5000;
 
-
-  app.listen(port, '0.0.0.0', () => {
-    logger.info(`Server listening on PORT ${port}`);
-
-    // Setup cron job
-    if (process.env.EMAIL) {
-      try {
-        // Schedule the cron job
-        schedule("*/20 * * * * *", async () => { await email_service.handleEmailCron() });
-
-        // Log success message for the cron job
-        logger.info('Mail job is active and scheduled to run every 30 seconds.');
-      } catch (error) {
-        // Log failure message if scheduling the cron job fails
-        logger.error('Mail job setup failed: ', error);
-      }
-    } else {
-      logger.info('No MAIL_USER specified, cron job not scheduled.');
-    }
+  app.listen(port, HOST, () => {
+    logger.info(`Server listening securely on HOST ${HOST} and PORT ${port}`);
+    logger.info(
+      "Email cron execution is handled by an external service via /email-cron.",
+    );
   });
 };
-
 
 const redisClient = redisSetup();
 module.exports = { startServer, redisClient };
