fff00f21fa29b980aff06577272009098f2a9f2f
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2026-07-25 21:37:08 +0100

    UPDATES: added google oauth wrapper to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import config from './config';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={config.googleClientId}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
