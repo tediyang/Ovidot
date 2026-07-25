db67617d627511b25467599d2a99ac5975b206cd
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2026-07-25 21:22:26 +0100

    UPDATES: increase the signin frame height

--- frontend/src/components/SigninImage.jsx
+++ frontend/src/components/SigninImage.jsx
@@ -4,7 +4,7 @@ import cycleImage from "../assets/signup/cycle.png";
 const SigninImage = () => {
   return (
     <div
-      className="lg:basis-1/2 flex flex-col justify-center items-center relative bg-cover bg-center bg-no-repeat min-h-full lg:min-h-[42.5rem] xl:rounded-tl-3xl xl:rounded-bl-3xl w-[100dvw] lg:max-w-[55rem]"
+      className="lg:basis-1/2 flex flex-col justify-center items-center relative bg-cover bg-center bg-no-repeat min-h-full lg:min-h-[47.5rem] xl:rounded-tl-3xl xl:rounded-bl-3xl w-[100dvw] lg:max-w-[55rem]"
       style={{ backgroundImage: `url(${cycleImage})` }}
     >
       <img src={logo_light} alt="logo" className="w-[100px] lg:w-[120px] xl:w-[150px] z-10 opacity-50 lg:opacity-100" />
